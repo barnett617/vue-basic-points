e8e020cbf4742345307bd457bc16d630fef90feb
wangxin <binaryaoe@163.com>
2021-07-22 22:54:29 +0800

    feat: 增加位置服务

---
+++ src/views/Loc.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h2>位置信息</h2>
+    <table>
+      <tbody>
+        <tr v-for="row of rows" :key="row.label">
+          <td>{{row.label}}</td>
+          <td>{{row.value}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      rows: [],
+      labelMap: {
+        country: '国家',
+        regionName: '省',
+        city: '市',
+        lat: '维度',
+        lon: '经度',
+        isp: '网络提供商',
+        query: 'IP地址'
+      },
+      uselessKeys: ['status', 'countryCode', 'region', 'zip', 'timezone', 'org', 'as']
+    }
+  },
+  mounted() {
+    this.getLoc();
+  },
+  methods: {
+    getLoc() {
+      let url = 'http://47.92.228.193:8990/'
+      fetch(url).then(res => res.json()).then(res => {
+        console.log('res', res);
+        if (+res.errno === 0) {
+          const obj = res.data
+          this.rows = Object.keys(obj)
+          .filter(key => !this.uselessKeys.includes(key))
+          .map(key => {
+            return {
+              label: this.labelMap[key],
+              value: obj[key]
+            }
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+table {
+  margin: auto;
+  tr {
+    border: 1px solid gray;
+  }
+  td {
+    border: 1px solid goldenrod;
+  }
+}
+</style>
+
