19ffd8d2070d3bb98ba06884bac990585af55f7c
wangxin <binaryaoe@163.com>
2021-07-23 20:47:40 +0800

    feat(vue.config.js): set root

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-const githubPages = 'https://barnett617.github.io/vue-basic-points/'
+const githubPages = 'https://www.techdemo.top/'
+// const githubPages = 'https://barnett617.github.io/vue-basic-points/'
 // const projectRoot = '/vue-basic-points/'
 
 module.exports = {
